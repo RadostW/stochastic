d43d8f207c23367b4f5abf83091be0c07f76936c
Radost Waszkiewicz <radost.waszkiewicz@gmail.com>
2020-10-05 17:34:47 +0200

    Type in near wall mobility function fit

--- main.cpp
+++ main.cpp
@@ -234,6 +234,30 @@ tdouble sqrt(tdouble x)
 }
 
 
+
+
+tdouble mobility(tdouble distance)
+{
+    tdouble x = 1.0 / distance;
+    return 0.986292 - x - 0.00688*cos(10.86762 + 8.092*x) + 0.02057*sin(2.506 + x*(3.074 + 2.227*x));
+}
+tdouble dmobility(tdouble distance)
+{
+    tdouble x = 1.0 / distance;
+    return -1.0 + (0.06323218 + 0.09161878*x)*cos(2.506 + x*(3.074 + 2.227*x)) + 0.05567296*sin(10.86762 + 8.092*x);
+}
+
+tdouble a_term(tdouble x)
+{
+    return dmobility(x);
+}
+tdouble b_term(tdouble x)
+{
+    return sqrt(mobility(x));
+}
+// stochastic equation to be solved:
+// dX = a_term(x) dt + b_term(x) dW
+
 int main()
 {
     tdouble x(1,0);
