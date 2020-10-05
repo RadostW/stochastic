f21cb4447bdf4ad761768b6f0bed37b83d1a045d
Radost Waszkiewicz <radost.waszkiewicz@gmail.com>
2020-10-05 19:07:15 +0200

    Typed in Wagner-Platen

--- main.cpp
+++ main.cpp
@@ -286,10 +286,10 @@ class ItoProcess
         normal = normal_distribution<double>(0.0,1.0);
     }
 
-    vector<double> SampleEuler( tdouble x0, double tmax , double dt)
+    vector<double> SampleEuler( double x0, double tmax , double dt)
     {
         double t=0;
-        tdouble x=tdouble(x0.get_value(),0);
+        tdouble x=tdouble(x0,0);
         vector<double> res;
         
         for(int i=0;dt*i < tmax;i++)
@@ -302,10 +302,10 @@ class ItoProcess
         }       
     }
 
-    vector<double> SampleMilstein( tdouble x0, double tmax , double dt)
+    vector<double> SampleMilstein( double x0, double tmax , double dt)
     {
         double t=0;
-        tdouble x=tdouble(x0.get_value(),0);
+        tdouble x=tdouble(x0,0);
         vector<double> res;
         
         for(int i=0;dt*i < tmax;i++)
@@ -319,6 +319,41 @@ class ItoProcess
             x = tdouble(x.get_value() + a*dt + b*dW + 0.5*b*bp*(dW*dW - dt),0);
         }
     }
+
+    vector<double> SampleWagnerPlaten( double x0, double tmax , double dt)
+    {
+        double t=0;
+        tdouble x=tdouble(x0,0);
+        vector<double> res;
+        
+        for(int i=0;dt*i < tmax;i++)
+        {
+            res.push_back(x.get_value());
+            tdouble faval = fa(x);
+            double a = faval.get_value();
+            double ap = faval.get_gradient()[0];
+            double app = faval.get_hessian()[0][0];
+
+            tdouble fbval = fb(x);
+            double b = fbval.get_value();
+            double bp = fbval.get_gradient()[0];
+            double bpp = fbval.get_hessian()[0][0];
+
+            double z1 = draw_normal();
+            double z2 = draw_normal();
+            double dW = z1*sqrt(dt);
+            double dZ = 0.5*(z1+z2/sqrt(3))*dt*sqrt(dt);
+
+
+            x = tdouble(
+                        x.get_value() + a*dt + b*dW + 0.5*b*bp*(dW*dW - dt) + 
+                        b*ap*dZ + 0.5*(a*ap+0.5*b*b*app)*dt*dt + 
+                        (a*bp+0.5*b*b*bpp)*(dW*dt-dZ) +
+                        0.5*b*(b*bpp+bp*bp)*((1./3.)*dW*dW-dt)*dW
+                        ,0);
+        }
+    }
+
 
     private:
     tdouble (*fa)(tdouble);
