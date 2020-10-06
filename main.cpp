3459a7933353591d37e6a9c0bfc0299fed538cc4
Radost Waszkiewicz <radost.waszkiewicz@gmail.com>
2020-10-06 16:54:50 +0200

    Add ceiling to equation

--- main.cpp
+++ main.cpp
@@ -6,17 +6,26 @@
 
 
 
-tdouble mobility(tdouble distance)
+const double ceiling = 50.;
+tdouble mobility(tdouble location)
 {
-    tdouble x = 1.0 / distance;
-    return 0.986292 - x - 0.00688*cos(10.86762 + 8.092*x) + 0.02057*sin(2.506 + x*(3.074 + 2.227*x));
+    tdouble x = 1.0 / location;
+    tdobule mobdown = 0.986292 - x - 0.00688*cos(10.86762 + 8.092*x) + 0.02057*sin(2.506 + x*(3.074 + 2.227*x));
+    x = 1.0 / (ceiling - location);
+    tdouble mobup = 0.986292 - x - 0.00688*cos(10.86762 + 8.092*x) + 0.02057*sin(2.506 + x*(3.074 + 2.227*x));
+    return location > (0.5*ceiling) ? mobup : mobdown;
 }
-tdouble dmobility(tdouble distance)
+tdouble dmobility(tdouble location)
 {
-    tdouble h = distance;
-    return ((-0.09161878 - 0.06323218*h)*
+    tdouble h = location;
+    tdouble dmobdown =  ((-0.09161878 - 0.06323218*h)*
       cos(2.506 + (2.227 + 3.074*h)/(h*h)) + 
      h*(1. - 0.05567296*sin(10.86762 + 8.092/h)))/(h*h*h);
+    h = ceiling - location;
+    tdouble dmobup =  -((-0.09161878 - 0.06323218*h)*
+      cos(2.506 + (2.227 + 3.074*h)/(h*h)) + 
+     h*(1. - 0.05567296*sin(10.86762 + 8.092/h)))/(h*h*h);
+    return location > (0.5*ceiling) ? dmobup : dmobdown;
 }
 
 tdouble a_term(tdouble x)
