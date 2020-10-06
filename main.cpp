4aa6976c374fe2ad1fd494ed78b2f11ccf54cf80
Radost Waszkiewicz <radost.waszkiewicz@gmail.com>
2020-10-06 14:51:00 +0200

    Fix simulated equation

--- main.cpp
+++ main.cpp
@@ -258,8 +258,10 @@ tdouble mobility(tdouble distance)
 }
 tdouble dmobility(tdouble distance)
 {
-    tdouble x = 1.0 / distance;
-    return -1.0 + (0.06323218 + 0.09161878*x)*cos(2.506 + x*(3.074 + 2.227*x)) + 0.05567296*sin(10.86762 + 8.092*x);
+    tdouble h = distance;
+    return ((-0.09161878 - 0.06323218*h)*
+      cos(2.506 + (2.227 + 3.074*h)/(h*h)) + 
+     h*(1. - 0.05567296*sin(10.86762 + 8.092/h)))/(h*h*h);
 }
 
 tdouble a_term(tdouble x)
@@ -268,7 +270,7 @@ tdouble a_term(tdouble x)
 }
 tdouble b_term(tdouble x)
 {
-    return sqrt(mobility(x));
+    return sqrt(2*mobility(x));
 }
 // stochastic equation to be solved:
 // dX = a_term(x) dt + b_term(x) dW
@@ -371,9 +373,18 @@ class ItoProcess
 int main()
 {
     ItoProcess proc = ItoProcess(a_term,b_term);
-    auto res =  proc.SampleEuler(3,5,0.1);
-
-    for (vector<double>::iterator i = res.begin(); i != res.end(); i++){
-    std::cout << *i << ' ';
+    auto res =  proc.SampleEuler(5,50,0.1);
+    for (vector<double>::iterator i = res.begin(); i != res.end(); i++)
+    {
+        std::cout << *i << ' ';
     }
+
+    cout<<"\n\n";
+    res =  proc.SampleMilstein(5,50,0.1);
+    for (vector<double>::iterator i = res.begin(); i != res.end(); i++)
+    {
+        std::cout << *i << ' ';
+    }
+
+
 }
