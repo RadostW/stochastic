357ce0119c6c70c01977b2bb486391a72476c558
Radost Waszkiewicz <radost.waszkiewicz@gmail.com>
2020-10-07 12:59:15 +0200

    Add double integral device, rewrite WagnerPlaten integrator

--- itoprocess.cpp
+++ itoprocess.cpp
@@ -1,8 +1,8 @@
-#include<iostream>
-#include<random>
-#include<vector>
-#include<cmath>
-#include<map>
+#include <iostream>
+#include <random>
+#include <vector>
+#include <cmath>
+#include <map>
 
 using namespace std;
 
@@ -10,32 +10,32 @@ class ItoProcess
 {
     // Used for obtaining trajectories from equation of type
     // dX = a(x) dt + b(x) dW
-    public:
-    ItoProcess( tdouble nfa(tdouble),tdouble nfb(tdouble) )
+public:
+    ItoProcess(tdouble nfa(tdouble), tdouble nfb(tdouble))
     {
         fa = nfa;
         fb = nfb;
-        normal = normal_distribution<double>(0.0,1.0);
-        WeinerPath[0.]=0.;
+        normal = normal_distribution<double>(0.0, 1.0);
+        WeinerPath[0.] = 0.;
+        ZetReported[0.] = 0.;
     }
     double WeinerValue(double t)
     {
-        if(t < 0)
+        if (t < 0)
         {
             return 0;
         }
-        else if(WeinerPath.count(t) == 1) // Repeated ask for the same value
+        else if (WeinerPath.count(t) == 1) // Repeated ask for the same value
         {
             return WeinerPath[t];
         }
-        
-        
+
         auto lower = WeinerPath.lower_bound(t);
-        if(lower == WeinerPath.end()) // Beyond rightmost sample
+        if (lower == WeinerPath.end()) // Beyond rightmost sample
         {
             lower--;
             double z = DrawNormal();
-            WeinerPath[t] = sqrt(t-lower->first)*z+lower->second;
+            WeinerPath[t] = sqrt(t - lower->first) * z + lower->second;
             return WeinerPath[t];
         }
         else
@@ -46,53 +46,86 @@ class ItoProcess
     void WeinerResample()
     {
         WeinerPath.clear();
-        WeinerPath[0.]=0.;
+        WeinerPath[0.] = 0.;
+        ZetReported.clear();
+        ZetReported[0.] = 0;
     }
-    
-
-    vector<double> SampleEuler( double x0, double tmax , double dt)
+    double ZetValue(double a, double b)
     {
-        double t=0;
-        tdouble x=tdouble(x0,0);
+        // Return first irreducible double integral:
+        // int_a^b int_a^q ds dW_q
+        auto lower = ZetReported.lower_bound(a);
+        auto upper = ZetReported.upper_bound(b);
+        if (lower == ZetReported.end())
+        {
+            throw logic_error("Weiner extrapolation not implemented");
+        }
+        else if (next(lower) == ZetReported.end() && upper == ZetReported.end())
+        {
+            double dW = WeinerValue(b) - WeinerValue(a);
+            double dt = b - a;
+
+            double z1 = dW * (1. / sqrt(b - a));
+            double z2 = DrawNormal();
+
+            double dZ = 0.5 * (z1 + z2 / sqrt(3)) * dt * sqrt(dt);
+            ZetReported[b] = dZ;
+            return dZ;
+        }
+        else if (lower->first == a && upper->first == b && next(lower) == upper)
+        {
+            // Alreay reported this interval, report again
+            return upper->second;
+        }
+        else
+        {
+            throw logic_error("Weiner subsampling not implemented");
+        }
+    }
+
+    vector<double> SampleEuler(double x0, double tmax, double dt)
+    {
+        double t = 0;
+        tdouble x = tdouble(x0, 0);
         vector<double> res;
-        
-        for(int i=0;dt*i < tmax;i++)
+
+        for (int i = 0; dt * i < tmax; i++)
         {
             res.push_back(x.get_value());
             double a = fa(x).get_value();
             double b = fb(x).get_value();
-            double dW = WeinerValue((i+1)*dt)-WeinerValue(i*dt);
-            x = tdouble(x.get_value() + a*dt + b*dW,0);
+            double dW = WeinerValue((i + 1) * dt) - WeinerValue(i * dt);
+            x = tdouble(x.get_value() + a * dt + b * dW, 0);
         }
-        return res;       
+        return res;
     }
 
-    vector<double> SampleMilstein( double x0, double tmax , double dt)
+    vector<double> SampleMilstein(double x0, double tmax, double dt)
     {
-        double t=0;
-        tdouble x=tdouble(x0,0);
+        double t = 0;
+        tdouble x = tdouble(x0, 0);
         vector<double> res;
-        
-        for(int i=0;dt*i < tmax;i++)
+
+        for (int i = 0; dt * i < tmax; i++)
         {
             res.push_back(x.get_value());
             double a = fa(x).get_value();
             tdouble fbval = fb(x);
             double b = fbval.get_value();
             double bp = fbval.get_gradient()[0];
-            double dW = WeinerValue((i+1)*dt)-WeinerValue(i*dt);
-            x = tdouble(x.get_value() + a*dt + b*dW + 0.5*b*bp*(dW*dW - dt),0);
+            double dW = WeinerValue((i + 1) * dt) - WeinerValue(i * dt);
+            x = tdouble(x.get_value() + a * dt + b * dW + 0.5 * b * bp * (dW * dW - dt), 0);
         }
         return res;
     }
 
-    vector<double> SampleWagnerPlaten( double x0, double tmax , double dt)
+    vector<double> SampleWagnerPlaten(double x0, double tmax, double dt)
     {
-        double t=0;
-        tdouble x=tdouble(x0,0);
+        double t = 0;
+        tdouble x = tdouble(x0, 0);
         vector<double> res;
-        
-        for(int i=0;dt*i < tmax;i++)
+
+        for (int i = 0; dt * i < tmax; i++)
         {
             res.push_back(x.get_value());
             tdouble faval = fa(x);
@@ -107,30 +140,28 @@ class ItoProcess
 
             double z1 = DrawNormal();
             double z2 = DrawNormal();
-            double dW = WeinerValue((i+1)*dt)-WeinerValue(i*dt);
-            z1 = dW*(1/sqrt(dt));
-            double dZ = 0.5*(z1+z2/sqrt(3))*dt*sqrt(dt);
-
-
+            double dW = WeinerValue((i + 1) * dt) - WeinerValue(i * dt);
+            double dZ = ZetValue(i * dt, (i + 1) * dt);
+            
             x = tdouble(
-                        x.get_value() + a*dt + b*dW + 0.5*b*bp*(dW*dW - dt) + 
-                        b*ap*dZ + 0.5*(a*ap+0.5*b*b*app)*dt*dt + 
-                        (a*bp+0.5*b*b*bpp)*(dW*dt-dZ) +
-                        0.5*b*(b*bpp+bp*bp)*((1./3.)*dW*dW-dt)*dW
-                        ,0);
+                x.get_value() + a * dt + b * dW + 0.5 * b * bp * (dW * dW - dt) +
+                    b * ap * dZ + 0.5 * (a * ap + 0.5 * b * b * app) * dt * dt +
+                    (a * bp + 0.5 * b * b * bpp) * (dW * dt - dZ) +
+                    0.5 * b * (b * bpp + bp * bp) * ((1. / 3.) * dW * dW - dt) * dW,
+                0);
         }
         return res;
     }
 
-    private:
+private:
     tdouble (*fa)(tdouble);
     tdouble (*fb)(tdouble);
     default_random_engine generator;
     normal_distribution<double> normal;
     double DrawNormal()
-    {   
+    {
         return normal(generator);
     }
-    map<double,double> WeinerPath;
-    map<double,double> ZetReported;
+    map<double, double> WeinerPath;
+    map<double, double> ZetReported; // Values of reported I(0,1) integral at given intervals, saved on rhs.
 };
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include<vector>
-#include<stdio.h>
+#include <vector>
+#include <stdio.h>
 #include "pmath.cpp"
 #include "tdouble.cpp"
 #include "itoprocess.cpp"
@@ -8,22 +8,24 @@ const double ceiling = 50.;
 tdouble mobility(tdouble location)
 {
     tdouble x = 1.0 / location;
-    tdouble mobdown = 0.986292 - x - 0.00688*cos(10.86762 + 8.092*x) + 0.02057*sin(2.506 + x*(3.074 + 2.227*x));
+    tdouble mobdown = 0.986292 - x - 0.00688 * cos(10.86762 + 8.092 * x) + 0.02057 * sin(2.506 + x * (3.074 + 2.227 * x));
     x = 1.0 / (ceiling - location);
-    tdouble mobup = 0.986292 - x - 0.00688*cos(10.86762 + 8.092*x) + 0.02057*sin(2.506 + x*(3.074 + 2.227*x));
-    return location > (0.5*ceiling) ? mobup : mobdown;
+    tdouble mobup = 0.986292 - x - 0.00688 * cos(10.86762 + 8.092 * x) + 0.02057 * sin(2.506 + x * (3.074 + 2.227 * x));
+    return location > (0.5 * ceiling) ? mobup : mobdown;
 }
 tdouble dmobility(tdouble location)
 {
     tdouble h = location;
-    tdouble dmobdown =  ((-0.09161878 - 0.06323218*h)*
-      cos(2.506 + (2.227 + 3.074*h)/(h*h)) + 
-     h*(1. - 0.05567296*sin(10.86762 + 8.092/h)))/(h*h*h);
+    tdouble dmobdown = ((-0.09161878 - 0.06323218 * h) *
+                            cos(2.506 + (2.227 + 3.074 * h) / (h * h)) +
+                        h * (1. - 0.05567296 * sin(10.86762 + 8.092 / h))) /
+                       (h * h * h);
     h = ceiling - location;
-    tdouble dmobup =  -((-0.09161878 - 0.06323218*h)*
-      cos(2.506 + (2.227 + 3.074*h)/(h*h)) + 
-     h*(1. - 0.05567296*sin(10.86762 + 8.092/h)))/(h*h*h);
-    return location > (0.5*ceiling) ? dmobup : dmobdown;
+    tdouble dmobup = -((-0.09161878 - 0.06323218 * h) *
+                           cos(2.506 + (2.227 + 3.074 * h) / (h * h)) +
+                       h * (1. - 0.05567296 * sin(10.86762 + 8.092 / h))) /
+                     (h * h * h);
+    return location > (0.5 * ceiling) ? dmobup : dmobdown;
 }
 
 tdouble a_term(tdouble x)
@@ -32,36 +34,33 @@ tdouble a_term(tdouble x)
 }
 tdouble b_term(tdouble x)
 {
-    return sqrt(2*mobility(x));
+    return sqrt(2 * mobility(x));
 }
 // stochastic equation to be solved:
 // dX = a_term(x) dt + b_term(x) dW
 
-
-
 int main()
 {
 
-    FILE* out;
-    out = fopen("toplot.dat","w");
+    FILE *out;
+    out = fopen("toplot.dat", "w");
 
-    ItoProcess proc = ItoProcess(a_term,b_term);
+    ItoProcess proc = ItoProcess(a_term, b_term);
 
     //proc.WeinerValue(5.);
     double x0 = 10;
     double tmax = 100;
     double dt = 1;
-    auto res1 =  proc.SampleMilstein(x0,tmax,dt);
-    auto res2 =  proc.SampleMilstein(x0,tmax,dt);
-    auto res3 =  proc.SampleMilstein(x0,tmax,dt);
+    auto res1 = proc.SampleWagnerPlaten(x0, tmax, dt);
+    auto res2 = proc.SampleMilstein(x0, tmax, dt);
+    auto res3 = proc.SampleMilstein(x0, tmax, dt);
     proc.WeinerResample();
-    auto res4 =  proc.SampleMilstein(x0,tmax,dt);
-    auto res5 =  proc.SampleMilstein(x0,tmax,dt);
+    auto res4 = proc.SampleMilstein(x0, tmax, dt);
+    auto res5 = proc.SampleMilstein(x0, tmax, dt);
 
-    for (int i=0; i<res1.size();i++)
+    for (int i = 0; i < res1.size(); i++)
     {
-        fprintf(out,"%lf %lf %lf %lf %lf\n",res1[i],res2[i],res3[i],res4[i],res5[i]);
+        fprintf(out, "%lf %lf %lf %lf %lf\n", res1[i], res2[i], res3[i], res4[i], res5[i]);
     }
-    cout<<"Done"<<endl;
-
+    cout << "Done" << endl;
 }
