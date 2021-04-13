d67b47f50d99d3c292efff40e044081a54bc4dac
Radost Waszkiewicz <radost.waszkiewicz@gmail.com>
2021-04-13 19:58:38 +0200

    fft

---
+++ fft.cpp
@@ -0,0 +1,36 @@
+// FFT code ripped from https://rosettacode.org/wiki/Fast_Fourier_transform#C.2B.2B
+// GFD 1.2 license
+
+#pragma once
+#include <complex>
+#include <iostream>
+#include <valarray>
+
+const double PI = 3.141592653589793238460;
+
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+
+// Cooley–Tukey FFT (in-place, divide-and-conquer)
+// Higher memory requirements and redundancy although more intuitive
+void fft(CArray& x)
+{
+    const size_t N = x.size();
+    if (N <= 1) return;
+
+    // divide
+    CArray even = x[std::slice(0, N/2, 2)];
+    CArray  odd = x[std::slice(1, N/2, 2)];
+
+    // conquer
+    fft(even);
+    fft(odd);
+
+    // combine
+    for (size_t k = 0; k < N/2; ++k)
+    {
+        Complex t = std::polar(1.0, -2 * PI * k / N) * odd[k];
+        x[k    ] = even[k] + t;
+        x[k+N/2] = even[k] - t;
+    }
+}
