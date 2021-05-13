c818f12551dd489aeef8595388245e3d1f24d31c
Maciej Bartczak <mb384493@students.mimuw.edu.pl>
2021-05-13 14:37:31 +0200

    commit

---
+++ test.py
@@ -0,0 +1,19 @@
+from paczka.problems.kp_4_27 import KloedenPlaten4_27
+from paczka.sde_solver import SDESolver
+
+problem = KloedenPlaten4_27()
+solver = SDESolver()
+solver.dt = 0.01
+solution = solver.solve(problem)
+
+exact = problem.exact_solution(problem.x0, solution['time_values'], solution['wiener_values'])
+
+import matplotlib.pyplot as plt
+plt.plot(solution['time_values'], solution['solution_values'], marker='x', label='simulated')
+plt.plot(solution['time_values'], exact, label='true')
+plt.legend()
+plt.savefig('sol.png')
+
+plt.plot(solution['time_values'], solution['solution_values']-exact)
+plt.title('Error')
+plt.savefig('error.png')
