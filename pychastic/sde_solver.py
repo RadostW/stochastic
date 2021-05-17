a020474deb2faf5b359cb0d2cd40dba812dba7df
Radost Waszkiewicz <radost.waszkiewicz@gmail.com>
2021-05-17 17:37:36 +0200

    VectorWiener -> forward sampling and recall, docs VectorSDESolver -> euler method, docs VectorSDEProblem -> groundwork, docs

--- pychastic/sde_solver.py
+++ pychastic/sde_solver.py
@@ -2,8 +2,10 @@ import jax
 import time
 import numpy as np
 from pychastic.sde_problem import SDEProblem
+from pychastic.sde_problem import VectorSDEProblem
 from pychastic.wiener import Wiener
 from pychastic.wiener import WienerWithZ
+from pychastic.wiener import VectorWiener
 
 
 class SDESolver:
@@ -29,15 +31,16 @@ class SDESolver:
          Target mean square error density used in variable step integrators.
 
     '''
-    def __init__(self):
-        self.adaptive = False
-        self.scheme = 'euler'  # euler | milstein
-        self.dt = 0.01
-        self.min_dt = self.dt/10
-        self.max_dt = self.dt*10
-        self.seed = None
-        self.error_terms = 1
-        self.target_mse_density = 0.1
+    def __init__(self, adaptive = False, scheme = 'euler', dt = 0.01, min_dt = 0.001,
+                 max_dt = 0.1, seed = None, error_terms = 1,target_mse_density = 0.1):
+        self.adaptive = adaptive
+        self.scheme = scheme  # euler | milstein
+        self.dt = dt
+        self.min_dt = min_dt
+        self.max_dt = max_dt
+        self.seed = seed
+        self.error_terms = error_terms
+        self.target_mse_density = target_mse_density
 
     def get_step_function(self, problem):
       if self.scheme == 'euler':
@@ -63,7 +66,10 @@ class SDESolver:
         raise KeyError('wrong scheme')
 
       step = jax.jit(step)
-      step(1.0, 1.0, 1.0)  # to compile
+      if self.scheme == 'wagner_platen':
+            step(1.0, 1.0, 1.0, 1.0)  # to compile
+      else:
+            step(1.0, 1.0, 1.0)  # to compile
       return step
 
     def get_optimal_dt_function(self, problem):
@@ -163,6 +169,7 @@ class SDESolver:
             w += dw
             if self.scheme == 'wagner_platen':
                 dz = wiener.get_z(t,t+dt)
+                print((x, dt, dw, dz))
                 x = step(x, dt, dw, dz)
             else:
                 x = step(x, dt, dw)
@@ -181,4 +188,105 @@ class SDESolver:
             wiener_values=np.array(wiener_values),
         )
 
+class VectorSDESolver:
+    '''
+    Produces realisations of stochastic process to ``solve`` method.
+    Controls numerical integration features via attributes.
 
+    Parameters
+    ----------
+    scheme : {'euler', 'commutative_milstein', 'milstein'}, default: 'euler'
+         Type of scheme used for integration.
+    dt : float
+         Step size in fixed-step integration.
+
+    '''
+    def __init__(self, scheme = 'euler', dt = 0.01):
+        self.scheme = scheme  # euler | milstein
+        self.dt = dt
+
+    def get_step_function(self, problem):
+        if self.scheme == 'euler':
+            def step(x, dt, dw):
+                return x + problem.a(x)*dt + np.dot(problem.b(x),dw)
+        elif self.scheme == 'commutative_milstein':
+            def step(x, dt, dw, comm_noise):
+                raise NotImplementedError
+        elif self.scheme == 'milstein':
+            def step(x, dt, dw, double_integrals):
+                raise NotImplementedError
+        else:
+            raise KeyError('Unknown scheme name')
+
+        #jax.jit(step) # ####### TODO #########
+
+        return step
+
+    def solve(self, problem: VectorSDEProblem, wiener: VectorWiener = None):
+        '''
+        Produce one realisation of the process specified by ``problem``.
+
+        Parameters
+        ----------
+        problem : VectorSDEProblem
+             Vector stochastic differential equation together with bounary conditions.
+        wiener : VectorWiener, optional
+             Underlying Wiener processes supplying noise to the equation. Usefull when comparing several solvers or several equations on the same noise.
+
+        Returns
+        -------
+        dict
+            Dictionary containing 2 entries. Under key ``time_values`` a np.array of timestamps on which process was evaluated.
+            Under key ``solution_values`` a np.array of stochastic process values at corresponding time instances.
+
+        Example
+        -------
+        >>> import numpy as np
+        >>> problem = pychastic.sde_problem.VectorSDEProblem(lambda x: np.array([1,1]), lambda x: np.array([[1,0.5],[0.5,1]]), 2, 2, np.array([1.5,0.5]), 1)
+        >>> solver = pychastic.sde_solver.VectorSDESolver()
+        >>> solver.solve(problem)
+        {'time_values': array([0.,0.01,...]), 'solution_values' : array([[1.5, 0.5], [1.76, 0.71], [1.93, 0.91], ...])} #some values random
+
+        '''
+        if self.scheme == 'euler':
+            wiener = wiener or VectorWiener(problem.noiseterms)
+        elif self.scheme == 'commutative_milstein':
+            raise NotImplementedError
+        elif self.scheme == 'milstein':
+            raise NotImplementedError
+        else:
+            raise KeyError('Unknown scheme name: '+str(self.scheme))
+
+        step = self.get_step_function(problem)
+
+        dt = self.dt
+        t = 0.0
+        x = problem.x0
+
+        # initialize "trajectories"
+        time_values = [0.0]
+        solution_values = [x]
+
+        while True:
+            w_prev = wiener.get_w(t)
+            w_next = wiener.get_w(t+dt) # Careful to sample in correct order!
+            dw = w_next - w_prev
+            t += dt
+
+            if self.scheme == 'euler':
+                x = step(x, dt, dw)
+            elif self.scheme == 'commutative_milstein':
+                raise NotImplementedError
+            elif self.scheme == 'milstein':
+                raise NotImplementedError
+
+            solution_values.append(x)
+            time_values.append(t)
+
+            if t >= problem.tmax:
+                break
+
+        return dict(
+            time_values=np.array(time_values),
+            solution_values=np.array(solution_values)
+        )
