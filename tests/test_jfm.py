bb7843013051d88703e2fa88e8386da3c53ed0b8
Radost Waszkiewicz <radost.waszkiewicz@gmail.com>
2021-08-24 13:33:51 +0200

    Improve test_jfm.py

--- tests/test_jfm.py
+++ tests/test_jfm.py
@@ -31,13 +31,14 @@ problem = pychastic.sde_problem.VectorSDEProblem(
       x0 = jnp.reshape(jnp.array([[-2.,0.,0.],[2.,0.,0.],[6.,0.,0.],[10.,0.,0.]]),(3*n_beads,)), # four beads
       dimension = 3*n_beads,
       noiseterms = 3*n_beads,
-      tmax = 10.0)
+      tmax = 100.0)
 
-solver = pychastic.sde_solver.VectorSDESolver(dt = 0.0001)
-trajectory = solver.solve(problem) # takes about 10 seconds
+solver = pychastic.sde_solver.VectorSDESolver(dt = 0.01)
+trajectories = np.array([solver.solve(problem) for x in range(100)])
 #plt.plot(trajectory['time_values'],trajectory['solution_values'][:,0])
 #plt.plot(trajectory['time_values'],trajectory['solution_values'][:,3])
-sol=trajectory['solution_values'];
-plt.plot(trajectory['time_values'],np.sum((sol[:,0:3]-sol[0,0:3])**2,axis=1))
+sol =  np.array([x['solution_values'] for x in trajectories]);
+plt.loglog(trajectories[0]['time_values'],np.sum(np.sum((sol[:,:,0:3]-sol[:,0,np.newaxis,0:3])**2,axis=2),axis=0)) # big bead
+plt.loglog(trajectories[0]['time_values'],np.sum(np.sum((sol[:,:,9:12]-sol[:,0,np.newaxis,9:12])**2,axis=2),axis=0)) # small bead
 plt.show()
 
