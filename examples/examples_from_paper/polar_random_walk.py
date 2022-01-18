187461c8fd680919ea98c0a34a87635f128af709
Radost Waszkiewicz <radost.waszkiewicz@gmail.com>
2022-01-18 13:17:02 +0100

    WIP Bias estimation for paper.

--- examples/examples_from_paper/polar_random_walk.py
+++ examples/examples_from_paper/polar_random_walk.py
@@ -7,39 +7,34 @@ problem = pychastic.sde_problem.SDEProblem(
     b=lambda x: jnp.array(
         [[jnp.cos(x[1]), jnp.sin(x[1])], [-jnp.sin(x[1]) / x[0], jnp.cos(x[1]) / x[0]]]
     ),
-    x0=jnp.array([1.0, 0.0]),
+    x0=jnp.array([3.0, 0.0]),
     tmax=1.0,
 )
 
 solver = pychastic.sde_solver.SDESolver()
 solver.scheme = 'euler'
 
-dts = [2**(-x) for x in range(12,18)]
-n_samples = 2**10
+dts = [2**(-x) for x in range(0,10,2)]
+n_samples = 2**25
 
-theoretical_mean = 1.548572460551145
-theoretical_variance = 0.7296709882254283
+theoretical_mean = 3.172577287900717
+theoretical_variance = 0.934753
 theoretical_sigma = np.sqrt(theoretical_variance / n_samples)
 
 bias_dict = dict()
 
-for dt in dts:
-    expected_r = np.zeros(2**6)
-    for i, _ in enumerate(expected_r):
-        solver.dt = dt
-        solution = solver.solve_many(problem,n_samples,seed=i)
-        terminal_r = solution['solution_values'][:,-1,0]
-        terminal_phi = solution['solution_values'][:,-1,1]
-        expected_r[i] = float(jnp.mean(terminal_r))
-        std_mean_r = float(jnp.std(terminal_r) / jnp.sqrt(n_samples))
-        print(f'{expected_r=}')
-        print(f'{std_mean_r=}')
-        print(f'{theoretical_mean=}')
-        print(f'{theoretical_sigma=}')
-        print(f'bias={expected_r[i]-theoretical_mean}')
-    print(f'bias={np.mean(expected_r)-theoretical_mean}')    
-    bias_dict[dt]=np.mean(expected_r)-theoretical_mean
+for dt in dts:    
+    solver.dt = dt
+    solution = solver.solve_many(problem,n_samples,seed=0)
+    terminal_r = solution['solution_values'][:,-1,0]
+    terminal_phi = solution['solution_values'][:,-1,1]
+    expected_r = float(jnp.mean(terminal_r))
+    std_mean_r = float(jnp.std(terminal_r) / jnp.sqrt(n_samples))
     
-for k,v in bias_dict.items():
-    print('{'+f'{k},{v}'+'},')
-
+    true_r = jnp.sum((solution['wiener_values'][:,-1]+problem.x0)**2,axis=1)**0.5
+    print(f'{expected_r - theoretical_mean}')
+    #print(f'{true_r=}')
+    #print(f'{terminal_r=}')
+    #print(f'error={terminal_r-true_r}')
+    #print(f'max_error={jnp.max(terminal_r-true_r)}')
+    #print(f'mean_error={jnp.mean(terminal_r-true_r)}')
