63fca948efb0e05dacd56303ba10c3a3ed24b8bf
Radost Waszkiewicz <radost.waszkiewicz@gmail.com>
2022-01-26 12:43:59 +0100

    Add sliding_differnce test

--- tests/test_sliding_window.py
+++ tests/test_sliding_window.py
@@ -22,3 +22,25 @@ def test_sliding_sum():
         for j in range(dim2):
             for k in range(dim2):
                 assert res_mat[i,j,k] = test_mat[i,j+k]
+                
+                
+def test_sliding_difference():
+    dim1 = 3
+    dim2 = 5
+
+    test_vec = jnp.arange(dim1)
+    test_mat = jnp.arange(dim1*dim2).reshape((dim1,dim2))
+    
+    res_vec = pychastic.vectorized_I_generation.sliding_sum(test_vec)
+    
+    for i in range(dim1):
+        for j in range(dim1):
+            assert res_vec[i,j] = test_vec[i-j]
+            
+            
+    res_mat = pychastic.vectorized_I_generation.sliding_sum(test_vec)
+    
+    for i in range(dim1):
+        for j in range(dim2):
+            for k in range(dim2):
+                assert res_mat[i,j,k] = test_mat[i,j-k]                
