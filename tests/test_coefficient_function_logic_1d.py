9c9044e02b077131e34e069f3b99e9156fc80334
Radost Waszkiewicz <radost.waszkiewicz@gmail.com>
2022-03-01 13:06:46 +0100

    Remove unused imports and black.

--- tests/test_coefficient_function_logic_1d.py
+++ tests/test_coefficient_function_logic_1d.py
@@ -1,21 +1,16 @@
-import jax
 import jax.numpy as jnp
 import pychastic.sde_problem
-from functools import wraps
 
 # Check logic inside sde_solver
 # Code is copied because L_t L_w and L are local variables and are hard to test
 # Such design is chosen because they depend on problem.a and problem.b
 
 problem = pychastic.sde_problem.SDEProblem(
-a = lambda x : jnp.exp(3.*x),
-b = lambda x : jnp.exp(7.*x),
-tmax = 1.0,
-x0 = jnp.array(0.)
+    a=lambda x: jnp.exp(3.0 * x),
+    b=lambda x: jnp.exp(7.0 * x),
+    tmax=1.0,
+    x0=jnp.array(0.0),
 )
-
-from pychastic.sde_solver import tensordot1
-from pychastic.sde_solver import tensordot2
 
 from pychastic.sde_solver import L_t_operator
 from pychastic.sde_solver import L_w_operator
@@ -27,10 +22,12 @@ dimension, noise_terms = problem.b(problem.x0).shape
 
 
 def L_t(f):
-    return L_t_operator(f,problem)
+    return L_t_operator(f, problem)
+
 
 def L_w(f):
-    return L_w_operator(f,problem)
+    return L_w_operator(f, problem)
+
 
 def L(f, idx):
     for x in reversed(idx):
@@ -41,21 +38,23 @@ def L(f, idx):
         else:
             raise ValueError
     return f
-    
+
+
 def test_L_operator_logic():
-    f = lambda x : x
+    f = lambda x: x
     x0 = jnp.array([0.0])
 
-    f_t = L(f,"t")(x0).squeeze() # should be a
-    f_w = L(f,"w")(x0).squeeze() # should be b
-    f_ww = L(f,"ww")(x0).squeeze() # should be b*b'
-    f_tw = L(f,"tw")(x0).squeeze() # should be a*b' + 0.5*b*b''
-    f_wt = L(f,"wt")(x0).squeeze() # should be b*a'
-    
-    sample_coefficients = jnp.array([f_t,f_w,f_ww,f_tw,f_wt])
-    target_coefficients = jnp.array([1.,1.,7.,7.+0.5*7.*7.,3.])
-    
-    assert jnp.allclose(sample_coefficients,target_coefficients)
-        
+    f_t = L(f, "t")(x0).squeeze()  # should be a
+    f_w = L(f, "w")(x0).squeeze()  # should be b
+    f_ww = L(f, "ww")(x0).squeeze()  # should be b*b'
+    f_tw = L(f, "tw")(x0).squeeze()  # should be a*b' + 0.5*b*b''
+    f_wt = L(f, "wt")(x0).squeeze()  # should be b*a'
+
+    sample_coefficients = jnp.array([f_t, f_w, f_ww, f_tw, f_wt])
+    target_coefficients = jnp.array([1.0, 1.0, 7.0, 7.0 + 0.5 * 7.0 * 7.0, 3.0])
+
+    assert jnp.allclose(sample_coefficients, target_coefficients)
+
+
 if __name__ == "__main__":
-    test_L_operator_logic()        
+    test_L_operator_logic()
