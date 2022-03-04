ec70bb6ff8fd0ee21c51a4c4d596495d2164f3ea
Radost Waszkiewicz <radost.waszkiewicz@gmail.com>
2022-03-04 13:47:17 +0100

    WIP High dimensional tests

--- examples/published_problems/cichocki_rubin_niedzwiecka_szymczak_jfm_2019.py
+++ examples/published_problems/cichocki_rubin_niedzwiecka_szymczak_jfm_2019.py
@@ -51,24 +51,87 @@ problem = pychastic.sde_problem.SDEProblem(
       #x0 = jnp.reshape(jnp.array([[-2.,0.,0.]]),(3*n_beads,)), # SINGLE BEAD BENCHMARK
       #dimension = 3*n_beads,
       #noiseterms = 3*n_beads,
-      tmax = 10000.0)
+      tmax = 2000.0)
 
-solver = pychastic.sde_solver.SDESolver(dt = 0.1)
-trajectories = np.array([solver.solve(problem) for x in tqdm(range(2))])
+def moving_average(a, n):
+    ret = jnp.cumsum(a)
+    return (ret[n:] - ret[:-n])/n
+    
+def trace_mobility(x):
+    locations = jnp.reshape(x,(n_beads,3))
+    mu = pygrpy.jax_grpy_tensors.muTT(locations,radii).reshape(n_beads,3,n_beads,3)
+    trace_mu = jnp.einsum('aibi -> ab', mu) # particle-wise trace
+    return trace_mu
+    
+def optimal_weights(x):
+    v_trace_mobility = jax.vmap(trace_mobility)
+    trace_mu = jnp.mean(v_trace_mobility(x),axis=0)
+    inv_trace_mu = jnp.linalg.inv(trace_mu)
+    return jnp.sum(inv_trace_mu,axis=-1) / jnp.sum(inv_trace_mu)
+    
+solver = pychastic.sde_solver.SDESolver(dt = 0.05)
+chunk_size = 1
+trajectories = solver.solve_many(problem,n_trajectories = 2**10, chunk_size = chunk_size, chunks_per_randomization = 1)
+
+weights = optimal_weights(trajectories["solution_values"][:,-1])
+
+big_bead_displacement = trajectories["solution_values"][:,:,0:3] - trajectories["solution_values"][:,jnp.newaxis,0,0:3]
+big_bead_distance = jnp.sum(big_bead_displacement**2,axis=-1)**0.5
+big_bead_msd = jnp.mean(big_bead_distance**2,axis=0)
+big_bead_instant_diffusion = jnp.ediff1d(big_bead_msd, to_end = float('nan')) / (solver.dt*chunk_size)
+
+small_bead_displacement = trajectories["solution_values"][:,:,9:12] - trajectories["solution_values"][:,jnp.newaxis,0,9:12]
+small_bead_distance = jnp.sum(small_bead_displacement**2,axis=-1)**0.5
+small_bead_msd = jnp.mean(small_bead_distance**2,axis=0)
+small_bead_instant_diffusion = jnp.ediff1d(small_bead_msd, to_end = float('nan')) / (solver.dt*chunk_size)
+
+(s_len,t_len,d_len) = trajectories["solution_values"].shape
+centre_trajectories = jnp.sum(trajectories["solution_values"].reshape(s_len,t_len,n_beads,3) * weights.reshape(1,1,n_beads,1),axis=2)
+centre_displacement = centre_trajectories[:,:,:] - centre_trajectories[:,0,jnp.newaxis,:]
+centre_distance = jnp.sum(centre_displacement**2,axis=-1)**0.5
+centre_msd = jnp.mean(centre_distance**2,axis=0)
+centre_instant_diffusion = jnp.ediff1d(centre_msd, to_end = float('nan')) / (solver.dt*chunk_size)
 
 
 #
 # plotting
 #
 
-sol =  np.array([x['solution_values'] for x in trajectories]);
-plt.plot(trajectories[0]['time_values'],(1.0/len(trajectories))*np.sum(np.sum((sol[:,:,0:3]-sol[:,0,np.newaxis,0:3])**2,axis=2),axis=0),label='First, big bead') # big bead
-plt.plot(trajectories[0]['time_values'],(1.0/len(trajectories))*np.sum(np.sum((sol[:,:,9:12]-sol[:,0,np.newaxis,9:12])**2,axis=2),axis=0),label='Last, small bead') # small bead
-plt.plot([0.0,problem.tmax],[0.0,0.3333*(1.0/ma.pi)*problem.tmax]) # largest bead -- theory
-plt.plot([0.0,problem.tmax],[0.0,0.2898*(1.0/ma.pi)*problem.tmax]) # BD sim Cichocki et al -- theory
+window = 100
+
+plt.plot(
+    moving_average(trajectories["time_values"][0],n=window),
+    moving_average(big_bead_instant_diffusion,n=window)
+    )
+
+plt.plot(
+    moving_average(trajectories["time_values"][0],n=window),
+    moving_average(small_bead_instant_diffusion,n=window)
+    )
+    
+plt.plot(
+    moving_average(trajectories["time_values"][0],n=window),
+    moving_average(centre_instant_diffusion,n=window)
+    )    
+
+def beta_regression(a,b):
+    return (jnp.corrcoef(jnp.array([a,b]))[0,1]) * jnp.std(b) / jnp.std(a)
+
+big_bead_apparent = beta_regression(trajectories["time_values"][0],big_bead_msd)
+small_bead_apparent = beta_regression(trajectories["time_values"][0],small_bead_msd)
+centre_apparent = beta_regression(trajectories["time_values"][0],centre_msd)
+
+print(f"{big_bead_apparent=}\n{small_bead_apparent=}\n{centre_apparent=}")
+
+#plt.plot(trajectories[0]["time_values"],(1.0/len(trajectories))*np.sum(np.sum((sol[:,:,0:3]-sol[:,0,np.newaxis,0:3])**2,axis=2),axis=0),label='First, big bead') # big bead
+#plt.plot(trajectories[0]["time_values"],(1.0/len(trajectories))*np.sum(np.sum((sol[:,:,9:12]-sol[:,0,np.newaxis,9:12])**2,axis=2),axis=0),label='Last, small bead') # small bead
+# plt.plot([0.0,problem.tmax],[0.0,0.3333*(1.0/ma.pi)*problem.tmax]) # largest bead -- theory
+#plt.plot([0.0,problem.tmax],[0.0,0.2898*(1.0/ma.pi)*problem.tmax]) # BD sim Cichocki et al -- theory
+plt.plot([0.0,problem.tmax],[0.2898*(1.0/ma.pi),0.2898*(1.0/ma.pi)]) # BD sim Cichocki et al -- theory
+plt.plot([0.0,problem.tmax],[0.2919*(1.0/ma.pi),0.2919*(1.0/ma.pi)]) # clever approximation Cichocki et al -- theory
 plt.xlabel(r"Dimensionless time ($t/\tau$)")
 plt.ylabel(r"Mean square displacement ($\mathbb{E}|q|^2$)")
-plt.legend()
+#plt.legend()
 
 plt.show()
 
